ef7fc781e4258bb3516d97e23c24754f6738c449
Ajit Singh <79194669+ajit199@users.noreply.github.com>
2022-06-19 19:54:20 +0530

    backend ready

---
+++ Back_End/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const productRouter = require("./routes/products");
+const authRouter = require("./routes/auth");
+let mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use("/auth", authRouter);
+app.use("/products", productRouter);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB.");
+});
+
+app.get("/", (req, res) => {
+  res.send("App Working");
+});
+let PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Listening On Port 8000");
+});
